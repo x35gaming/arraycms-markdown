44905863f439fe5493f1f917f2f5f72fed740287
x35gaming <44137552+x35gaming@users.noreply.github.com>
2019-03-22 19:00:17 +0800

    Update markdown.php

--- markdown.php
+++ markdown.php
@@ -1,5 +1,5 @@
 <?php
-  require("libs/Parsedown.php")
+  require("libs/Parsedown.php");
   $Parsedown = new Parsedown();
   function mdblog() {
     $blog=array($Parsedown->text('Hello _Parsedown_!'));
